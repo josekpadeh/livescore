e375ed2714bd9387b8527c729be069fba8213efb
I. Jose Kpadeh <ijosekpadeh@gmail.com>
2022-12-15 12:06:41 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { SmartPhone } from './Smartphone';
+import { phoneContent } from './phoneContent';
+
+
+function App() {
+  return (
+    <>
+    <SmartPhone/>
+    <phoneContent/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/phoneContent.js
@@ -0,0 +1,8 @@
+import React from "react";
+function phoneContent(){
+    return(
+        <div className="container">
+        <h2 id="project-title">Phonebook App</h2>
+      </div>
+    )
+}
